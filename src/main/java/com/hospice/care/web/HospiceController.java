be5c914384821f2debc3ffb47151098f0ac3112d
taehun0023 <taehunn0023@gmail.com>
2022-07-13 18:54:54 +0900

    first upload

---
+++ src/main/java/com/hospice/care/web/HospiceController.java
@@ -0,0 +1,20 @@
+package com.hospice.care.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/")
+@Controller
+public class HospiceController {
+
+    @GetMapping(value = "/facility")
+    public String service() {
+        return "service/facility";
+    }
+
+    @GetMapping(value = "/test")
+    public String test() {
+        return "test";
+    }
+}
