54d4b980a545b1e6d4c99e9ea8bccdaf80c7830f
boreum <bobobo2927@gmail.com>
2022-07-22 14:42:36 +0900

    testupdate

--- src/main/java/com/hospice/care/web/PlaceController.java
+++ src/main/java/com/hospice/care/web/PlaceController.java
@@ -20,12 +20,6 @@ public class PlaceController {
         return "place/placeSaitama";
     }
 
-    @GetMapping(value = "/placeTokyo")
-    public String placeTokyo(){
-        System.out.println("/place/placeTokyo");
-        return "place/placeTokyo";
-    }
-
     @GetMapping(value = "/placeNishiba")
     public String placeNishiba(){
         System.out.println("/place/placeNishiba");
